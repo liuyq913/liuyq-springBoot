4ec691cf40f76808e0e9a27d7c436ee0ceb007cb
liuyqq <liuyq@cheok.com>
2019-06-09 15:42:56 +0800

    springcloud zuul权限校验

--- liuyq-springBoot-service/src/main/java/com/liuyq/boot/service/Task/MyTask.java
+++ liuyq-springBoot-service/src/main/java/com/liuyq/boot/service/Task/MyTask.java
@@ -1,30 +1,37 @@
 package com.liuyq.boot.service.Task;
 
 import java.util.TimerTask;
-import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * Created by liuyq on 2019/5/26.
  */
-public class MyTask extends TimerTask{
+public class MyTask extends TimerTask {
     private ThreadPoolExecutor executor;
-    private ScheduledExecutorService scheduler;
+    private final ScheduledExecutorService scheduler;
     private Runnable task;
 
 
-    public MyTask(ThreadPoolExecutor executor, ScheduledExecutorService scheduler, Runnable task){
+    public MyTask(ThreadPoolExecutor executor, ScheduledExecutorService scheduler, Runnable task) {
         this.executor = executor;
         this.scheduler = scheduler;
         this.task = task;
     }
+
     @Override
     public void run() {
-        Future future =  executor.submit(task);
         try {
-            System.out.println("mytask");
-            scheduler.shutdownNow();
+            executor.execute(task);
+            executor.execute(task);
+          //  System.out.println("是否完成任务1：-----"+future.isDone());
+           // System.out.println("是否完成任务2：-----"+future2.isDone());
+            System.out.println("要关闭任务了");
+            //shutdownNow 是通过中断来实现不执行任务的，这种方法的作用有限，如果线程中没有sleep 、wait、Condition、定时锁等应用,
+            // interrupt()方法是无法中断当前的线程的 ，showdownNow并不代表线程池就立即退出。
+            //List<Runnable> list =  executor.shutdownNow();
+           // System.out.println("关闭线程池之前，未开始执行的任务大小---"+list.size());
+            System.out.println("关闭了-----"+executor.isShutdown());
         } catch (Exception e) {
             e.printStackTrace();
 
--- liuyq-springBoot-service/src/main/java/com/liuyq/boot/service/Task/MyTask2.java
+++ liuyq-springBoot-service/src/main/java/com/liuyq/boot/service/Task/MyTask2.java
@@ -8,8 +8,11 @@ public class MyTask2 implements Runnable{
     public void run() {
         System.out.println("执行任务开始。。。");
         try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
+            Thread.sleep(1000);
+           for(int i= 0;i<1000;i++){
+               System.out.println(i);
+           }
+        } catch (Exception e) {
             e.printStackTrace();
         }
         System.out.println("执行结束。。。");
