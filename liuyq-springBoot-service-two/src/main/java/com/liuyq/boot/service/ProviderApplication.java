c9e87a585e7f81d98c895de635afef48879e5fa5
liuyqq <liuyq@cheok.com>
2019-06-10 19:17:41 +0800

    config 集成erueka

--- liuyq-springBoot-service-two/src/main/java/com/liuyq/boot/service/ProviderApplication.java
+++ liuyq-springBoot-service-two/src/main/java/com/liuyq/boot/service/ProviderApplication.java
@@ -4,15 +4,16 @@ package com.liuyq.boot.service;
  * Created by liuyq on 2019/5/13.
  */
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
+@MapperScan(basePackages = "com.liuyq.**.mapper")
 public class ProviderApplication {
 
     public static void main(String[] agrs){
--- liuyq-springBoot-service-two/src/main/java/com/liuyq/boot/service/controller/ControllerDemo.java
+++ liuyq-springBoot-service-two/src/main/java/com/liuyq/boot/service/controller/ControllerDemo.java
@@ -1,4 +1,4 @@
-package service.controller;
+package com.liuyq.boot.service.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
