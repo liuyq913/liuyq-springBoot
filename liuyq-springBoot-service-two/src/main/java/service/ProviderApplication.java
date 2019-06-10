a0e0d3bb701293c1fab5162692e433d218cbca34
liuyqq <liuyq@cheok.com>
2019-06-10 09:44:01 +0800

    config 集成erueka

--- liuyq-springBoot-service-two/src/main/java/service/ProviderApplication.java
+++ liuyq-springBoot-service-two/src/main/java/service/ProviderApplication.java
@@ -7,12 +7,11 @@ package service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
 public class ProviderApplication {
 
     public static void main(String[] agrs){
