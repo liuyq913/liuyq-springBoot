48da8aa6ca9711c66ae177f857c2ba4ca1f838b6
liuyuqing <liuyuqing@sright.com>
2020-11-29 23:39:32 +0800

    feat k8s单机版搭建完成，同时写部分yaml 文件生成pob

--- liuyq-springBoot-eureka/build.sh
+++ liuyq-springBoot-eureka/build.sh
@@ -1,5 +1,4 @@
 #-f :指定要使用的Dockerfile路径
 # -t XXX 构建tag为xxx 镜像
-docker build -f Dockerfile -t dockerhub.ergengtech.com/genghuo/genghuo-trade:dev .
-#docker push dockerhub.ergengtech.com/genghuo/genghuo-trade:dev
-#docker push liuyuqing1212/eureka:dev
+docker build -f Dockerfile -t liuyuqing1212/liuyq-springboot-eureka:dev .
+docker push liuyuqing1212/liuyq-springboot-eureka:dev
